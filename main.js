f1a57e2841e5594a4f1f17f0ca27503fdd17f7bc
TheTomik93 <thetomik93@gmail.com>
2022-10-09 23:02:21 +0200

    finalised game logic

--- main.js
+++ main.js
@@ -19,9 +19,10 @@ function game(){
     for (let i=0; i<3; i++){
         getComputerChoice();
         getPlayerChoice();
-        /*playRound();
-        recordScore();
-        logResults();*/
+        playRound(computerChoice, playerChoice);
+        computerScoreUpdate(lastRoundWinner);
+        playerScoreUpdate(lastRoundWinner);
+        reportScore(computerChoice, playerChoice, computerScore, playerScore);
     };
 }
 
@@ -58,4 +59,80 @@ function getPlayerChoice(){
         console.log("BAD INPUT, only rock paper scissors valid");
         getPlayerChoice(playerChoice);
     }
+}
+
+function playRound(computerChoice, playerChoice){
+    if(computerChoice == "ROCK"){
+        if(playerChoice == "ROCK"){
+            console.log("its a draw")
+            lastRoundWinner = 2;
+        }
+        else if(playerChoice == "PAPER"){
+            console.log("player wins")
+            lastRoundWinner = 1;
+        }
+        else if(playerChoice == "SCISSORS"){
+            console.log("computer wins")
+            lastRoundWinner = 0;
+        }
+    }
+    else if(computerChoice == "PAPER"){
+        if(playerChoice == "PAPER"){
+            console.log("its a draw")
+            lastRoundWinner = 2;
+        }
+        else if(playerChoice == "SCISSORS"){
+            console.log("player wins")
+            lastRoundWinner = 1;
+        }
+        else if(playerChoice == "ROCK"){
+            console.log("computer wins")
+            lastRoundWinner = 0;
+        }
+    }
+    else if(computerChoice == "SCISSORS"){
+        if(playerChoice == "SCISSORS"){
+            console.log("its a draw")
+            lastRoundWinner = 2;
+        }
+        else if(playerChoice == "ROCK"){
+            console.log("player wins")
+            lastRoundWinner = 1;
+        }
+        else if(playerChoice == "PAPER"){
+            console.log("computer wins")
+            lastRoundWinner = 0;
+        }
+    }
+    console.log(lastRoundWinner);
+    return lastRoundWinner;
+}
+
+function playerScoreUpdate(lastRoundWinner){
+    if(lastRoundWinner == 1){
+        playerScore++;
+        console.log("PLAYER SCORE INCREASED BY ONE");
+        return playerScore;
+    }
+    else{
+        return;
+    }
+}
+
+function computerScoreUpdate(lastRoundWinner){
+    console.log(lastRoundWinner);
+
+    if(lastRoundWinner == 0){
+        computerScore++;
+        console.log("COMPUTER SCORE INCREASED BY ONE");
+        return computerScore;
+    }
+    else{
+        return;
+    }
+}
+
+function reportScore(computerChoice, playerChoice, computerScore, playerScore){
+    console.log(`Computer played ${computerChoice}, player played ${playerChoice}`);
+    console.log(`Computer score: ${computerScore} / Player score: ${playerScore}`);
 }
